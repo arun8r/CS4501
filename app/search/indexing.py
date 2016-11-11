7952b953f6e479b6719dde02957b7568770cdb4d
arun8r <arun8r@gmail.com>
2016-11-10 21:39:07 -0500

    proj5

--- app/search/indexing.py
+++ app/search/indexing.py
@@ -2,15 +2,13 @@ from kafka import KafkaConsumer
 from kafka import KafkaProducer
 from elasticsearch import Elasticsearch
 import json
+import time
 
-print ("hey whats up")
 
+time.sleep(20)
 elasticsearch = Elasticsearch([{'host': 'elasticsearch', 'port': 9200}])
 consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
 for message in consumer:
     new_sale = (json.loads((message.value).decode('utf-8')))
-    
-    print(message)
     ret = elasticsearch.index(index='listing-indexer',doc_type = 'listing',id = new_sale['id'])
     elasticsearch.indices.refresh(index="listing-indexer")
-    print("hello from the indexer")
