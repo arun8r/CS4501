fa164d191f72b6939c9ffcaca648bf6d95a5e92b
Maxwell Luo <maxwellluo@Maxwells-MacBook-Pro.local>
2016-12-11 23:54:05 -0500

    set up spark in docker-compose and get basic script running

---
+++ data/generate.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_log(file, num_lines, num_products, num_users):
+    with open(file, 'w') as f:
+        for i in range(num_lines):
+            user_id = random.randint(1, num_users)
+            product_id = random.randint(1, num_products)
+            f.write('%s\t%s\n' % (user_id, product_id))
+
+generate_log("access.log", 300, 10, 20)
