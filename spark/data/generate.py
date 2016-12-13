0cc2ed05c1991b17c921cd2b5f6332b55d534356
Maxwell Luo <maxwellluo@Maxwells-MBP.home>
2016-12-12 20:15:46 -0500

    finalize spark python file

--- spark/data/generate.py
+++ spark/data/generate.py
@@ -7,4 +7,4 @@ def generate_log(file, num_lines, num_products, num_users):
             product_id = random.randint(1, num_products)
             f.write('%s\t%s\n' % (user_id, product_id))
 
-generate_log("access.log", 300, 10, 20)
+generate_log("access.log", 20, 5, 5)
